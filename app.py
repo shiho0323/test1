f00215349e824b7ecf53ec1273cc58254e500c9d
shiho0323 <167150743+shiho0323@users.noreply.github.com>
2025-03-30 18:47:58 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+
+data = pd.read_csv("/Users/hiramatsushiho/Desktop/野球部作業場/トレ系/トレ全体4.csv")
+meibo = pd.read_csv("/Users/hiramatsushiho/Desktop/野球部作業場/名簿/24trackman.csv")
+
+meibo = meibo.query("PitcherTeam == 'TOK'")
+
+st.title("トレ＆体重データ")
+
+page = st.sidebar.radio("表示モードを選択", ("個人表示", "全体表示"))
+
+if page == "個人表示":
+    st.header("個人別データ表示")
+    # ユーザー名の選択
+    names = ["全員"] + list(meibo["フルネーム"].unique())
+    selected_name = st.sidebar.selectbox("名前を選択", options=names)
+    
+    # フィルター処理
+    if selected_name != "全員":
+        filtered_data = data[data["名前"] == selected_name]
+        date = filtered_data["日付"].tolist()
+        weight = filtered_data["体重"].tolist()
+        plt.plot(date, weight, label=selected_name)
+        plt.legend(loc="upper left", bbox_to_anchor=(1.05, 1))
+        plt.xticks(rotation=45)
+        plt.xlabel("日付")
+        plt.ylabel("体重")
+        st.pyplot()
+    else:
+        filtered_data = data
+    
+    st.subheader(f"{selected_name}のデータ" if selected_name != "全員" else "全ユーザーのデータ")
+    st.dataframe(filtered_data)
+
+elif page == "全体表示":
+    st.header("平均データ表示")
